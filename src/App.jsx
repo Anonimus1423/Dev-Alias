915d880127a2356840de0501b859bc727dce42ad
Rud <rudikhayk2017@gmail.com>
2022-05-08 19:50:21 +0400

    toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Toaster } from 'react-hot-toast';
 import styled from 'styled-components';
 import { Router } from './components/Router';
 
@@ -9,6 +10,30 @@ const StyledApp = styled.div`
 function App() {
   return (
     <StyledApp>
+      <Toaster
+          position="top-right"
+          reverseOrder={false}
+          gutter={8}
+          containerClassName=""
+          containerStyle={{}}
+          toastOptions={{
+            // Define default options
+            className: '',
+            duration: 5000,
+            style: {
+              background: '#363636',
+              color: '#fff',
+            },
+            // Default options for specific types
+            success: {
+              duration: 3000,
+              theme: {
+                primary: 'green',
+                secondary: 'black',
+              },
+            },
+          }}
+/>
       <Router />
     </StyledApp>
   );
--- src/components/settings-page/settings-page-components/AddPoints.jsx
+++ src/components/settings-page/settings-page-components/AddPoints.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import toast, { Toaster } from 'react-hot-toast';
 import styled from 'styled-components';
 import { DecrementIcon, IncrementIcon } from '../../../assets/icons/settings';
-
+import {changePoint} from '../../../store/reducers/point-reducer'
 
 const Points = styled.div`
     margin-top: 20px;
@@ -44,23 +45,43 @@ const EButton = styled.button`
     cursor:pointer;
     border: 1px solid ${({theme})=>theme.colors.quartersColor};
     color:${({theme})=>theme.colors.quartersColor};
-    padding: 3px;
+    path{   
+        fill: ${({theme})=>theme.colors.quartersColor} 
+    }
+    padding: 3px 5px;
+    transition: 0.3s;
+    &:active{
+        background: ${({theme})=>theme.colors.quartersColor};
+        path{
+            fill: ${({theme})=>theme.colors.secondColor};
+        }
+    }
 `
 
 export const AddPoints = () => {
     const LivePoints = useSelector(state=>state.point.points)
+    const dispatch = useDispatch()
+    
+    const ChangeLivePoints = (newPoints) => {
+        if(newPoints>0 && newPoints<360){
+            dispatch(changePoint(newPoints))
+        }else {
+            toast.error('Max Victory Points` 360')
+        }
+    }
+
     return(
         <Points>
             <PoinstTitle>
                 VICTORY POINTS
             </PoinstTitle>
             <PointBlock>
-                <PointInput type='number' value={LivePoints}/>
+                <PointInput type='number' value={LivePoints} onChange={(e)=>ChangeLivePoints(e.target.value)}/>
                 <EButtons>
-                    <EButton>
+                    <EButton onClick={()=>ChangeLivePoints(LivePoints+1)}>
                         <IncrementIcon/>
                     </EButton>
-                    <EButton>
+                    <EButton onClick={()=>ChangeLivePoints(LivePoints-1)}>
                         <DecrementIcon/>
                     </EButton>
                 </EButtons>
