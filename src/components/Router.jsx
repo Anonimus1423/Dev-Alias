94c172e84d396253540d4e46f7c0f5dc46f5f62b
Rud <rudikhayk2017@gmail.com>
2022-05-08 20:28:55 +0400

    errrrr

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -17,8 +17,8 @@ export const Router = () =>
   const [page, setPage] = useState(0);
   const dispatch = useDispatch()
   useEffect(()=>{
-    let PrevGame = JSON.parse(localStorage.getItem('game')) || []
-    if(PrevGame){
+    let PrevGame = JSON.parse(localStorage.getItem('game')) || {teams:[]}
+    if(PrevGame.teams.length){
       dispatch(localSet(PrevGame.teams.teams))
       dispatch(changePoint(PrevGame.point.points))
       dispatch(changeTime(PrevGame.time.time))
